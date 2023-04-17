220d0dd24543304842e1b12875def9dbacf91c20
Nicholas Tvaroha <nicholas.tvaroha@mymail.champlain.edu>
2023-04-17 16:39:34 -0400

    Finished testing script, doesn't work, going to check with Scott

--- main.c
+++ main.c
@@ -11,6 +11,8 @@ main(argc, argv)
 	int   argc;
 	char* argv[];
 {
+    int fd;
+
     if (argc > 1) {
         for (int i = 1; i < argc; i++) {
             pid_t pid = fork();
@@ -20,12 +22,51 @@ main(argc, argv)
             }
             else { //Child
                 //Open file
-                //For each line (repeat for allotted length)
-                    //read number of bytes in the line (not counting spaces?) (read until /n)
-                    //check if number of bytes is less or more than allotted width (including /n))
-                        //if less, just print line and then add # missing number of bytes
-                        //if more, print only up to allotted width
-                //if bytes_read is 0 or /0 occurs just print # allotted width times for the rest of the allotted length
+                if((fd = open(argv[pid], O_RDWR)) >= 0) //no idea how to choose which argv[] from the child
+	            {
+                    int height = 0;
+		            //For each line (repeat for allotted length)
+                    for (int i = 0; i < WIDTH; i++) {
+                        char* buf[];
+                        char* temp[];
+                        //read number of bytes in the line (not counting spaces?) (read until /n)
+                        int bytes_read = read(argv[pid], buf, WIDTH, 1);
+                        //check if number of bytes is less or more than allotted width (including /n))
+                        if (bytes_read == 0) { //if bytes_read is 0 or /0 occurs just print # allotted width times for the rest of the allotted length   
+                            for (int i = 0; i < WIDTH; i++) {
+                                temp[i] = '0';
+                            }
+                            temp[WIDTH] = '/n';
+                            write(fid, temp, WIDTH, 1);
+                        }
+                        else if (bytes_read == -1) {
+                            perror("read failed");
+                        }
+                        else if (bytes_read < WIDTH) { //if less, just print line and then add # missing number of bytes
+                            for (int i = 0; i < bytes_read; i++) {
+                                temp[i] = buf[i];
+                            }
+                            for (int i = bytes_read; i < WIDTH; i++) {
+                                temp[i] = '0';
+                            }
+                        }
+                        else { //if more, print only up to allotted width
+                            for (int i = 0; i < bytes_read - 1; i++) {
+                                temp[i] = buf[i];
+                            }
+                            temp[WIDTH] = '/n';
+                            write(fid, temp, WIDTH, 1);
+                        }
+                        height++;
+                    }
+
+		            close(fd);
+	            }
+	            else
+	                {
+		            perror("open(/dev/CSI230ASCII) failed");
+		            exit(1);
+	            }
             }
         }
     else {
