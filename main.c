bc8666e94ab32f020002bc17fc51f7e240322e23
Graham Finlayson-Fife <graham@finlayson-fife.org>
2023-04-20 05:33:31 -0400

    lint

--- main.c
+++ main.c
@@ -7,92 +7,105 @@
 
 #include "common.h"
 
-int
-main(argc, argv, envp)
-	int   argc;
-	char* argv[];
-    char* envp[];
+int main(argc, argv, envp)
+int argc;
+char *argv[];
+char *envp[];
 {
     int fd;
 
-    if (argc > 1) {
-        for (int i = 1; i < argc; i++) {
+    if (argc > 1)
+    {
+        for (int i = 1; i < argc; i++)
+        {
             pid_t pid = fork();
 
-            if (pid != 0) { //Parent
-                //Nothing goes here
+            if (pid != 0)
+            { // Parent
+                // Nothing goes here
             }
-            else { //Child
-                //Open file
-                if((fd = open(argv[1], O_RDWR)) >= 0)
-	            {
-                    char* buf = malloc(WIDTH * WIDTH);
-                    char* temp = malloc(WIDTH * WIDTH);
-                    //read number of bytes in the line (not counting spaces?) (read until \n)
-                    int bytes_read = read(fd, buf, (WIDTH*WIDTH));
+            else
+            { // Child
+                // Open file
+                if ((fd = open(argv[1], O_RDWR)) >= 0)
+                {
+                    char *buf = malloc(WIDTH * WIDTH);
+                    char *temp = malloc(WIDTH * WIDTH);
+                    // read number of bytes in the line (not counting spaces?) (read until \n)
+                    int bytes_read = read(fd, buf, (WIDTH * WIDTH));
 
-                    if (bytes_read == -1) {
+                    if (bytes_read == -1)
+                    {
                         perror("Read failed");
                     }
-                    else {
+                    else
+                    {
                         int current = 0;
-                        for(int i = 0; i < WIDTH; i++) {
-                            for (int j = 0; j < WIDTH; j++) {
+                        for (int i = 0; i < WIDTH; i++)
+                        {
+                            for (int j = 0; j < WIDTH; j++)
+                            {
                                 int slot = (i * WIDTH) + j;
-                                if(buf[current] != '\n') {
+                                if (buf[current] != '\n')
+                                {
                                     temp[slot] = buf[current];
                                 }
-                                else {
+                                else
+                                {
                                     for (int k = 0; k < WIDTH - j; k++)
                                         temp[slot + k] = '0';
-                                    //temp[slot + WIDTH - j] = '\n';
+                                    // temp[slot + WIDTH - j] = '\n';
                                     j = WIDTH;
                                 }
                                 current++;
 
-                                if (j == WIDTH - 1) { //check if at end of line in file, moves to next line
+                                if (j == WIDTH - 1)
+                                { // check if at end of line in file, moves to next line
                                     while (buf[current] != '\n')
-									{
-										current++;
-										printf("%s\n", &buf[current]);
-									}
+                                    {
+                                        current++;
+                                        printf("%s\n", &buf[current]);
+                                    }
                                     current++;
                                 }
                             }
                         }
-                        temp[WIDTH*WIDTH] = '\0';
+                        temp[WIDTH * WIDTH] = '\0';
 
-                        int bytes_written = write(fd, temp, (WIDTH*WIDTH));
+                        int bytes_written = write(fd, temp, (WIDTH * WIDTH));
                     }
-printf("%s\n", "Close");
-		            close(fd);
-	            }
-	            else
-	                {
-		            perror("open(/dev/CSI230ASCII) failed");
-		            exit(1);
-	            }
+                    printf("%s\n", "Close");
+                    close(fd);
+                }
+                else
+                {
+                    perror("open(/dev/CSI230ASCII) failed");
+                    exit(1);
+                }
             }
         }
 
-        //Print the new buffer
-        char* toPrint = malloc(WIDTH * WIDTH);
-        int bytes_read = read(fd, toPrint, (WIDTH*WIDTH));
+        // Print the new buffer
+        char *toPrint = malloc(WIDTH * WIDTH);
+        int bytes_read = read(fd, toPrint, (WIDTH * WIDTH));
         printf("%s\n", toPrint);
-
-    } else {
-        //fork then,
-        //execve
+    }
+    else
+    {
+        // fork then,
+        // execve
         pid_t pid = fork();
-        if (pid != 0) { //Parent
-            //Nothing goes here
+        if (pid != 0)
+        { // Parent
+            // Nothing goes here
         }
-        else { //Child
+        else
+        { // Child
             execve("genmap.sh", argv, envp);
-            //char* toPrint = malloc(WIDTH * WIDTH);
-            //size_t bytes_read = read(fd, toPrint, (WIDTH*WIDTH));
-            //toPrint[bytes_read] = '\0';
-            //printf("%s\n", toPrint);
+            // char* toPrint = malloc(WIDTH * WIDTH);
+            // size_t bytes_read = read(fd, toPrint, (WIDTH*WIDTH));
+            // toPrint[bytes_read] = '\0';
+            // printf("%s\n", toPrint);
         }
     }
     exit(0);
--- mapDriver.c
+++ mapDriver.c
@@ -1,76 +1,75 @@
 #include "mapDriver.h"
 
-char static_art[] = 
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
-;
+char static_art[] =
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n"
+	"-NT AL GF TW NT AL GF TW NT AL GF TW NT AL GF TW-\n";
 
 /* Driver's Status is kept here */
 static driver_status_t status =
-{
-	'a',   /* Starting ASCII char is '0' */
-	false, /* Not busy at the beginning */
-    2500,
-	{0},   /* buffer */
-	NULL,  /* buffer's ptr */
-	-1,    /* major */
-	-1     /* minor */
+	{
+		'a',   /* Starting ASCII char is '0' */
+		false, /* Not busy at the beginning */
+		2500,
+		{0},  /* buffer */
+		NULL, /* buffer's ptr */
+		-1,	  /* major */
+		-1	  /* minor */
 };
 
 /* This function is called whenever a process
  * attempts to open the device file
  */
 static int device_open(inode, file)
-	struct inode* inode;
-	struct file*  file;
+struct inode *inode;
+struct file *file;
 {
 	static int counter = 0;
 
@@ -85,18 +84,16 @@ static int device_open(inode, file)
 	status.minor = inode->i_rdev >> 8;
 	status.minor = inode->i_rdev & 0xFF;
 
-	printk
-	(
+	printk(
 		"Device: %d.%d, busy: %d\n",
 		status.major,
 		status.minor,
-		status.busy
-	);
+		status.busy);
 
 	/* We don't want to talk to two processes at the
 	 * same time
 	 */
-	if(status.busy)
+	if (status.busy)
 		return -EBUSY;
 
 	/* If this was a process, we would have had to be
@@ -124,13 +121,11 @@ static int device_open(inode, file)
 	status.busy = true;
 
 	/* Initialize the message. */
-	sprintf
-	(
+	sprintf(
 		status.buf,
 		"If I told you once, I told you %d times - %s",
 		counter++,
-		"Hello, world\n"
-	);
+		"Hello, world\n");
 
 	/* The only reason we're allowed to do this sprintf
 	 * is because the maximum length of the message
@@ -150,11 +145,11 @@ MODULE_LICENSE("GPL");
  * device file.
  */
 static int device_release(inode, file)
-	struct inode* inode;
-	struct file*  file;
+struct inode *inode;
+struct file *file;
 {
 #ifdef _DEBUG
-	printk ("device_release(%p,%p)\n", inode, file);
+	printk("device_release(%p,%p)\n", inode, file);
 #endif
 
 	/* We're now ready for our next caller */
@@ -167,22 +162,22 @@ static int device_release(inode, file)
  * have already opened the device file attempts to
  * read from it.
  */
-static ssize_t device_read(file, buffer, length, offset) //TODO
-	struct file* file;
-    char*        buffer;  /* The buffer to fill with data */
-    size_t       length;  /* The length of the buffer */
-    loff_t*      offset;  /* Our offset in the file */
+static ssize_t device_read(file, buffer, length, offset) // TODO
+struct file *file;
+char *buffer;	/* The buffer to fill with data */
+size_t length;	/* The length of the buffer */
+loff_t *offset; /* Our offset in the file */
 {
 	ssize_t bytes_read = 0;
 	int i;
 
-	if(length > status.buf_length)
+	if (length > status.buf_length)
 	{
 		printk("Reading failed: Requested more bytes than in buffer!");
 		return 0;
 	}
 
-	for(i = 0; i < length; ++i)
+	for (i = 0; i < length; ++i)
 	{
 		put_user(status.buf[i], &buffer[i]);
 		bytes_read++;
@@ -194,156 +189,153 @@ static ssize_t device_read(file, buffer, length, offset) //TODO
 /* This function is called when somebody tries to write
  * into our device file.
  */
-static ssize_t device_write(file, buffer, length, offset) //TODO
-	struct file* file;
-	const char*  buffer;  /* The buffer */
-	size_t       length;  /* The length of the buffer */
-	loff_t*      offset;  /* Our offset in the file */
+static ssize_t device_write(file, buffer, length, offset) // TODO
+struct file *file;
+const char *buffer; /* The buffer */
+size_t length;		/* The length of the buffer */
+loff_t *offset;		/* Our offset in the file */
 {
 	ssize_t bytes_written = 0;
 	int i;
 
-    if (length > status.buf_length)
-    {
-        printk("Writing Failed: Too large to fit in map\n");
-        return 0;
-    }
+	if (length > status.buf_length)
+	{
+		printk("Writing Failed: Too large to fit in map\n");
+		return 0;
+	}
 
-    for (i = 0; i < length; i++)
-    {
+	for (i = 0; i < length; i++)
+	{
 		get_user(status.buf[i], &buffer[i]);
-        status.buf_ptr = &status.buf[i];
+		status.buf_ptr = &status.buf[i];
 		bytes_written++;
-    }
-    return bytes_written;
+	}
+	return bytes_written;
 }
 
-static loff_t device_lseek(file, offset, whence) //TODO
-	struct file* file;
-	loff_t*      offset;  /* Our offset in the file */
-    int          whence;
+static loff_t device_lseek(file, offset, whence) // TODO
+struct file *file;
+loff_t *offset; /* Our offset in the file */
+int whence;
 {
-    loff_t temp;
+	loff_t temp;
 
-    switch(whence)
-    {
-        case SEEK_SET:
-			temp = *offset;
-            break;
-        case SEEK_CUR:
-            temp = *(status.buf_ptr + *offset);
-            break;
-        case SEEK_END:
-            temp = *(DRV_BUF_SIZE + offset);
-            break;
-        default:
-            return -EINVAL;
-    };
+	switch (whence)
+	{
+	case SEEK_SET:
+		temp = *offset;
+		break;
+	case SEEK_CUR:
+		temp = *(status.buf_ptr + *offset);
+		break;
+	case SEEK_END:
+		temp = *(DRV_BUF_SIZE + offset);
+		break;
+	default:
+		return -EINVAL;
+	};
 
-	if(temp > DRV_BUF_SIZE || temp < 0)
+	if (temp > DRV_BUF_SIZE || temp < 0)
 	{
 		return -EINVAL;
 	}
 
-    status.buf_ptr = (char*)temp;
-    return temp;
+	status.buf_ptr = (char *)temp;
+	return temp;
 }
 
-static ssize_t device_ioctl(file, cmd, arg) //TODO
-	struct file* file;
-	uint         cmd;
-	unsigned long arg;
+static ssize_t device_ioctl(file, cmd, arg) // TODO
+struct file *file;
+uint cmd;
+unsigned long arg;
 {
-    char* temp;
+	char *temp;
 
-    switch(cmd)
-    {
-        case 1:
-			memset(status.buf, 0, sizeof(status.buf));
-            break;
+	switch (cmd)
+	{
+	case 1:
+		memset(status.buf, 0, sizeof(status.buf));
+		break;
 
-        case 2:
-            memcpy(status.buf, static_art, sizeof(static_art));
-            status.buf_length = 2500;
-            status.buf_ptr = NULL;
-            break;
-        case 3:     
-            int width = 0;
-            int count = 0;
-            temp = status.buf;
-            while (*temp && *temp != '\n') {
-                width++;
-                temp++;
-            }
-            temp = status.buf;
+	case 2:
+		memcpy(status.buf, static_art, sizeof(static_art));
+		status.buf_length = 2500;
+		status.buf_ptr = NULL;
+		break;
+	case 3:
+		int width = 0;
+		int count = 0;
+		temp = status.buf;
+		while (*temp && *temp != '\n')
+		{
+			width++;
+			temp++;
+		}
+		temp = status.buf;
 
-            while(*temp) {
-                count++;
-                if(*temp == '\n') {
-                    count--;
-                    if (count != width) {
-                        printk("Line is shorter than width");
-                        return -1;
-                    }
-                    else {
-                        count = 0;
-                    }
-                }
-                else if (*temp < 32) {
-                    printk("A character is outside of the writable ascii");
-                    return -1;
-                }
-                temp++;
-            }
-            break;
-    };
+		while (*temp)
+		{
+			count++;
+			if (*temp == '\n')
+			{
+				count--;
+				if (count != width)
+				{
+					printk("Line is shorter than width");
+					return -1;
+				}
+				else
+				{
+					count = 0;
+				}
+			}
+			else if (*temp < 32)
+			{
+				printk("A character is outside of the writable ascii");
+				return -1;
+			}
+			temp++;
+		}
+		break;
+	};
 
-    return 0;
+	return 0;
 }
 
 /* Initialize the module - Register the character device */
 int init_module(void)
 {
 	/* Register the character device (atleast try) */
-	status.major = register_chrdev
-	(
+	status.major = register_chrdev(
 		0,
 		DEVICE_NAME,
-		&Fops
-	);
+		&Fops);
 
 	/* Negative values signify an error */
-	if(status.major < 0)
+	if (status.major < 0)
 	{
-		printk
-		(
+		printk(
 			"Sorry, registering the ASCII device failed with %d\n",
-			status.major
-		);
+			status.major);
 
 		return status.major;
 	}
 
-	printk
-	(
+	printk(
 		"Registeration is a success. The major device number is %d.\n",
-		status.major
-	);
+		status.major);
 
-	printk
-	(
-		"If you want to talk to the device driver,\n" \
-		"you'll have to create a device file. \n" \
-		"We suggest you use:\n\n" \
-		"mknod %s c %d <minor>\n\n" \
+	printk(
+		"If you want to talk to the device driver,\n"
+		"you'll have to create a device file. \n"
+		"We suggest you use:\n\n"
+		"mknod %s c %d <minor>\n\n"
 		"You can try different minor numbers and see what happens.\n",
 		DEVICE_NAME,
-		status.major
-	);
+		status.major);
 
 	return SUCCESS;
 }
-
 
 /* Cleanup - unregister the appropriate file from /proc */
 void cleanup_module(void)
