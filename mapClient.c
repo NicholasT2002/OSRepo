60aa75b0bd9e713239a095a7bf258fd61ee8f895
Aaron Lurie <aaronlurie@gmail.com>
2023-04-25 20:23:16 -0400

    Improved client and server

--- mapClient.c
+++ mapClient.c
@@ -15,28 +15,32 @@
 #include "common.h"
 #include "mapClient.h"
 
-int main(argc, argv)
-int argc;
-char *argv[];
+int main(int argc, char* argv[])
 {
     int sock_fd;
     char* ip = DEFAULT_IP;
     struct sockaddr_in server;
 
     if (argc > 1)
-        ip = argv[1];
+	{
+		ip = argv[1];
+	}
 
     //Create socket
     if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-        perror("Client Socket could not be created\n");
+	{
+		perror("Client Socket could not be created");
+	}
+        
 
     //Connect to server
     server.sin_addr.s_addr = inet_addr(ip);
     server.sin_family = AF_INET;
-    server.sin_port = DEFAULT_PORT;
+    server.sin_port = htons(DEFAULT_PORT);
 
-    if (connect(sock_fd, (struct sockaddr *)&server, sizeof(server)) < 0) {
-        perror("Client Connection Error\n");
+    if (connect(sock_fd, (struct sockaddr *)&server, sizeof(server)) < 0) 
+	{
+        perror("Client Connection Error");
         return 1;
     }
 
@@ -45,16 +49,21 @@ char *argv[];
     //char* mMessage = malloc(sizeof(char)+sizeof(int)*2); //For if Specifying width/height
 
     message = 'M' + (char*)0;
-    if (send(sock_fd, message, strlen(message), 0) < 0) {
-        perror("Client Sending failed\n");
+    if (send(sock_fd, message, sizeof(message), 0) < 0) 
+	{
+        perror("Client Sending failed");
         return 1;
     }
 
     //Receive Messages
     char* server_reply[3000];
+
     if (recv(sock_fd, server_reply, 3000, 0) < 0)
-        perror("Client Receving failed\n");
-    printf(server_reply);
+	{
+		perror("Client receiving failed");
+	}
+
+    printf("%s\n", *server_reply);
 
     //Close Socket
     close(sock_fd);
--- mapServer.c
+++ mapServer.c
@@ -27,8 +27,8 @@ char *argv[];
         ip = argv[1];
 
     //Create socket
-    if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-        perror("Could not create server socket\n");
+    if ((sock_fd = socket(AF_INET, SOCK_STREAM | O_NONBLOCK, 0)) < 0)
+        perror("Could not create server socket");
 
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = inet_addr(ip);
@@ -36,14 +36,19 @@ char *argv[];
 
     //Bind socket
     if (bind(sock_fd, (struct sockaddr*)&server, sizeof(server)) < 0)
-        perror("Server bind failed\n");
+        perror("Server bind failed");
 
     //Listen
     listen(sock_fd, 3);
 
     //Accept incoming connections
     c = sizeof(struct sockaddr_in);
-    while ((new_socket = accept(sock_fd, (struct sockaddr*)&client, (socklen_t*)&c))) {
+
+    while (new_socket = accept(sock_fd, (struct sockaddr*)&client, (socklen_t*)&c))
+	{
+		// Try again if no connections
+		if(errno == EAGAIN || errno == EWOULDBLOCK) { continue; }
+
         printf("Received Message\n");
         pid_t pid = fork();
 
@@ -51,95 +56,92 @@ char *argv[];
         { // Parent
             wait(pid);
         }
-        if (pid == 0)
+
+        else
         { // Child
             char* client_message = malloc(3000);
             char* server_reply = malloc(3000);
             char* err_msg = "Server failed to read client message";
-            if (recv(sock_fd, client_message, 3000, 0) < 0) {
+
+			// Try to get message, send error if we don't
+            if (recv(sock_fd, client_message, 3000, 0) < 0) 
+			{
                 server_reply[0] = 'E';
                 server_reply[1] = (char)sizeof(err_msg);
                 for (int i = 2; i < sizeof(err_msg) + 3; i++)
                     server_reply[i] = err_msg[i];
                 if (send(sock_fd, server_reply, strlen(server_reply), 0) < 0) {
-                    perror("Server Sending failed\n");
+                    perror("Server Sending failed");
                     return 1;
                 }
-                perror("Server Receving failed\n");
+                perror("Server Receving failed");
             }
-            else if (client_message[0] != 'M') {
+
+			// Send error if message is of incorrect format
+            else if (client_message[0] != 'M') 
+			{
                 server_reply[0] = 'E';
                 server_reply[1] = (char)sizeof(err_msg);
                 for (int i = 2; i < sizeof(err_msg) + 3; i++)
                     server_reply[i] = err_msg[i];
                 if (send(sock_fd, server_reply, strlen(server_reply), 0) < 0) {
-                    perror("Server Sending failed\n");
+                    perror("Server Sending failed");
                     return 1;
                 }
                 perror("Server Received non-M message\n");
             }
-            else {
-                if (client_message[1] == 0) {
-                    server_reply[0] = 'M';
-                    server_reply[1] = WIDTH;
-                    server_reply[2] = HEIGHT;
-                    char *buf = malloc(WIDTH * HEIGHT);
-                    int fd = open("/dev/mapDriver", O_RDWR);
-                    if (fd == -1)
-                    {
-                        perror("Error opening driver\n");
-                        exit(0);
-                    }
-                    
-                    int bytes_read = read(fd, buf, (WIDTH * HEIGHT));
-                    if (bytes_read == -1)
-                    {
-                        perror("Read failed\n");
-                    }
 
-                    for (int i = 3; i < bytes_read + 3; i++)
-                        server_reply[i] = buf[i - 3];
+            else 
+			{
+				int requestedWidth, requestedHeight;
 
-                    if (send(sock_fd, server_reply, strlen(server_reply), 0) < 0) {
-                        perror("Server Sending failed\n");
-                        return 1;
-                    }
-
-                    close(fd);
+				// If client gave no parameters, send default-sized map
+                if (client_message[1] == 0) 
+				{
+					requestedWidth = WIDTH;
+					requestedHeight = HEIGHT;
                 }
-                else {
-                    int clientWidth = client_message[3];
-                    int clientHeight = client_message[4];
-                    server_reply[0] = 'M';
-                    server_reply[1] = clientWidth;
-                    server_reply[2] = clientHeight;
-                    char *buf = malloc(clientWidth * clientHeight);
-                    int fd = open("/dev/mapDriver", O_RDWR);
-                    if (fd == -1)
-                    {
-                        perror("Error opening driver\n");
-                        exit(0);
-                    }
-                    
-                    int bytes_read = read(fd, buf, (clientWidth * clientHeight));
-                    if (bytes_read == -1)
-                    {
-                        perror("Read failed\n");
-                    }
 
-                    for (int i = 3; i < bytes_read + 3; i++)
-                        server_reply[i] = buf[i - 3];
-
-                    if (send(sock_fd, server_reply, strlen(server_reply), 0) < 0) {
-                        perror("Server Sending failed\n");
-                        return 1;
-                    }
-
-                    close(fd);
+				// Send map of size that client wants
+                else 
+				{
+                    requestedWidth = client_message[3];
+                    requestedHeight = client_message[4];
                 }
+
+                server_reply[0] = 'M';
+                server_reply[1] = requestedWidth;
+                server_reply[2] = requestedHeight;
+
+                char *buf = malloc(requestedWidth * requestedHeight);
+                int fd = open("/dev/mapDriver", O_RDWR);
+
+                if (fd == -1)
+                {
+                    perror("Error opening driver");
+                    exit(0);
+                }
+                
+                int bytes_read = read(fd, buf, (requestedWidth * requestedHeight));
+
+                if (bytes_read == -1)
+                {
+                    perror("Read failed");
+                }
+
+                for (int i = 3; i < bytes_read + 3; i++)
+                    server_reply[i] = buf[i - 3];
+
+                if (send(sock_fd, server_reply, strlen(server_reply), 0) < 0) 
+				{
+                    perror("Server Sending failed");
+                    return 1;
+                }
+
+                close(fd);
             }
         }
     }
 
-    exit(0);
+    return 0;
 }
